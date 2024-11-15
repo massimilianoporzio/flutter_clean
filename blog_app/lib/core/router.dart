99a002bbe7dcd12f40c4618e1bc6b8f1cef9c08c
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2024-11-15 11:26:31 +0100

    login signup

--- blog_app/lib/core/router.dart
+++ blog_app/lib/core/router.dart
@@ -1,30 +1,31 @@
 // GoRouter configuration
+import 'package:blog_app/features/auth/presentation/pages/signin_page.dart';
+import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class MyRouter {
   static GoRouter get router => _router;
   static final _router = GoRouter(
-    initialLocation: '/',
+    initialLocation: '/login',
     routes: [
       GoRoute(
         name: 'home',
         path: '/',
-        builder: (context, state) => Scaffold(
+        builder: (context, state) => const Scaffold(
           body: Placeholder(),
         ),
       ),
-      // GoRoute(
-      //   name:
-      //       'login', // Optional, add name to your routes. Allows you navigate by name instead of path
-      //   path: '/login',
-      //   builder: (context, state) => SignInPage(),
-      // ),
-      // GoRoute(
-      //   name: 'signup',
-      //   path: '/signup',
-      //   builder: (context, state) => SignUpPage(),
-      // ),
+      GoRoute(
+        name: 'login', // Optional, add name to your routes. Allows you navigate by name instead of path
+        path: '/login',
+        builder: (context, state) => const SignInPage(),
+      ),
+      GoRoute(
+        name: 'signup',
+        path: '/signup',
+        builder: (context, state) => const SignUpPage(),
+      ),
     ],
   );
 }
---
+++ blog_app/lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,103 @@
+import 'package:blog_app/app_palette.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+        onPressed: () {
+          context.goNamed('login');
+        },
+        icon: const Icon(Icons.arrow_back_ios),
+        //replace with our own icon data.
+      )),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign Up.',
+                style: GoogleFonts.padauk(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              AuthField(
+                hintText: "Name",
+                controller: nameController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              AuthField(
+                hintText: "Email",
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              AuthField(
+                hintText: "Password",
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const AuthGradientButton(buttonText: "Sign Up"),
+              const SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () => context.goNamed("login"),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Already have an account? ",
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                        TextSpan(
+                          text: 'Sign In',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium
+                              ?.copyWith(color: AppPalette.gradient2, fontWeight: FontWeight.bold),
+                        )
+                      ]),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
