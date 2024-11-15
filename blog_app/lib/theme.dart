7e1cd085eca79e39017f6f2ed48849363eef4421
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2024-11-15 10:34:41 +0100

    theme

---
+++ blog_app/lib/theme.dart
@@ -0,0 +1,15 @@
+import 'package:blog_app/app_palette.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static _border([Color color = AppPalette.borderColor]) =>
+      OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: color, width: 2));
+  static final darkThemeMode = ThemeData.dark().copyWith(
+      appBarTheme: const AppBarTheme(backgroundColor: AppPalette.backgroundColor),
+      scaffoldBackgroundColor: AppPalette.backgroundColor,
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.all(27),
+        enabledBorder: _border(),
+        focusedBorder: _border(AppPalette.gradient2),
+      ));
+}
